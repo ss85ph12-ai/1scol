ffa2e8ee3515fc4645ec3e4d9cdf0a224bb64df4
ss85ph12-ai <ss85ph12@gmail.com>
2026-05-31 12:47:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,18 @@
-// ملف الإعدادات العامة للتطبيق - يسهل عليك عمل نسخ متعددة من التطبيق
+// ملف الإعدادات المركزية للمدرسة
 const APP_CONFIG = {
-    // تم التعديل ليكون مفتاح قاعدة البيانات الخاص بمدرسة الرحمن
+    // 1. مفتاح قاعدة البيانات الخاص بالمدرسة
     DB_KEY: 'skol_al_rhman_db',
     
-    // إصدار التطبيق (قم بتغييره مستقبلاً لإجبار الأجهزة على التحديث)
-    APP_VERSION: 'v1'
+    // 2. إصدار التطبيق (قم بتغييره لإجبار الأجهزة على تحديث النظام)
+    APP_VERSION: 'v1',
+
+    // 3. إعدادات Firebase الخاصة بالنسخة
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyBBTgKrUmvmgfgdC_rvTjxnkBlsbu23gqA",
+        authDomain: "skol-al-rhman.firebaseapp.com",
+        projectId: "skol-al-rhman",
+        storageBucket: "skol-al-rhman.firebasestorage.app",
+        messagingSenderId: "757664712826",
+        appId: "1:757664712826:web:cb595e976cc47b9515d3c3"
+    }
 };
